789b8927b0851f938b39eebfbc52201e6dd1c64d
devHudi <devhudi@gmail.com>
2022-04-14 13:43:43 +0900

    reset.css 및 전역 스타일 적용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import { Routes, Route } from "react-router-dom";
+import { Reset } from "styled-reset";
+import { createGlobalStyle } from "styled-components";
 
 import HomePage from "./pages/HomePage";
 import ImagePage from "./pages/ImagePage";
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #fafafa;
+  }
+`;
+
+const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/:id" element={<ImagePage />} />
-    </Routes>
+    <>
+      <Reset />
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:id" element={<ImagePage />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
